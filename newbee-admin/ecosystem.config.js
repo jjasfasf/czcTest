ceed1c4a66cde91826311bae6ac114dd060419a4
CZC <478844801@qq.com>
2022-11-27 17:46:23 +0800

    test

--- newbee-admin/ecosystem.config.js
+++ newbee-admin/ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
       production: {
         user: 'root', // 通过root权限进入服务器
         host: '120.24.7.42', // 服务器ip地址
-        ref: 'origin/master', // 要拉取代码的分支
+        ref: 'origin/main', // 要拉取代码的分支
         repo: 'git@github.com:jjasfasf/czcTest.git', // 代码仓库地址，我存放的是 Github
         path: '/workspace/vue3-admin', // 拉取代码后，存放在服务器的文件地址。
         'post-deploy': 'git reset --hard && git checkout main && git pull && npm i --production=false && npm run build:release && pm2 startOrReload ecosystem.config.js', // 顺序执行这些指令，最后 pm2 startOrReload ecosystem.config.js 会通过 pm2 启动 vue3-admin-server.js
